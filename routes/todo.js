4e9028030c6a89df4019959decc4705972230633
Nikhil Polavarapu <np22@pdx.edu>
2023-03-18 21:35:49 -0700

    Modified code to resolve Console Error and allowing smooth Addition and Deletion of Tasks

--- routes/todo.js
+++ routes/todo.js
@@ -2,66 +2,74 @@ var express = require("express");
 var router = express.Router();
 var db = require("../models/index");
 const toDo = require("../models/todo")
-router.get("/",function(req,res){
-    db.toDo.find()
-      .then(function(todo){
-        res.json(todo);
-      })
-      .then(function(todos){
-          console.log(todos);
-      })
-      .catch(function(err){
-          console.log(err);
-      });   
-});
-router.put("/:todoId",function(req,res){
-    db.toDo.findByIdAndUpdate(req.params.todoId,req.body)
-    .then(function(todo){
-        res.json(todo);
-    })
-    .catch(function(err){
-        console.log(err);
-    })
-})
-router.post("/",function(req,res){
-    const task=req.body.name;
-    console.log("Task  :",task)
-    console.log(req.body)
-    console.log("Code reached post :",req.body)
-    //Todo.create(req.body)
-    const todo = new toDo(req.body)
-    todo.save((err, data) => {
-        if(err) {
-            console.log("Error saving in mongo db");
-            return;
-        }
-        console.log(data, "Data saved")
 
+router.get("/", function(req, res) {
+  db.toDo.find()
+    .then(function(todo) {
+      res.json(todo);
+      console.log(todo);
     })
-    // .then(function(newtodo){
-    //     console.log("Nww todo :",newtodo)
-    //     res.status(201).json(newtodo);
-    // })
-    // .catch(function(err){
-    //     console.log(err);
-    // })
-})
-router.get("/:todoId",function(req,res){
-    db.toDo.findById(req.params.todoId)
-    .then(function(foundtodo){
-        res.json(foundtodo);
-    })
-    .catch(function(err){
-        console.log(err);
-    })
-})
-router.delete("/:todoId",function(req,res){
-    db.toDo.findByIdAndRemove(req.params.todoId)
-    .then(function(){
-        res.json({message:"We deleted it"});
-    })
-    .catch(function(err){
-        console.log(err);
-    })
+    .catch(function(err) {
+      console.log(err);
+      res.status(500).json({error: "Internal server error"});
+    });   
 });
-module.exports=router;
+
+router.put("/:todoId", function(req, res) {
+  db.toDo.findByIdAndUpdate(req.params.todoId, req.body)
+    .then(function(todo) {
+      if (!todo) {
+        return res.status(404).json({error: "Todo not found"});
+      }
+      res.json(todo);
+    })
+    .catch(function(err) {
+      console.log(err);
+      res.status(500).json({error: "Internal server error"});
+    });
+});
+
+router.post("/", function(req, res) {
+  const task = req.body.name;
+  if (!task) {
+    return res.status(400).json({error: "Missing task name"});
+  }
+  toDo.create(req.body, function(err, newtodo) {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({error: "Error saving to database"});
+    }
+    console.log(newtodo, "Data saved")
+    res.status(201).json(newtodo);
+  });
+});
+
+router.get("/:todoId", function(req, res) {
+  db.toDo.findById(req.params.todoId)
+    .then(function(foundtodo) {
+      if (!foundtodo) {
+        return res.status(404).json({error: "Todo not found"});
+      }
+      res.json(foundtodo);
+    })
+    .catch(function(err) {
+      console.log(err);
+      res.status(500).json({error: "Internal server error"});
+    });
+});
+
+router.delete("/:todoId", function(req, res) {
+  db.toDo.findByIdAndRemove(req.params.todoId)
+    .then(function(deletedTodo) {
+      if (!deletedTodo) {
+        return res.status(404).json({error: "Todo not found"});
+      }
+      res.json({message: "TASK DELETED"});
+    })
+    .catch(function(err) {
+      console.log(err);
+      res.status(500).json({error: "Internal server error"});
+    });
+});
+
+module.exports = router;
